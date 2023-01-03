3e39741d45ab4861a6e01e68ad3fb83ae505df4f
Oleksii Ryzhov <oleksii.ryzhov@gmail.com>
2023-01-03 23:50:43 +0200

    some new trainings2

---
+++ src/JS/JS-lesson-31-callback.js
@@ -0,0 +1,29 @@
+// // setTimeout example
+
+// function first() {
+//   // do smthg
+//   setTimeout(function () { console.log(1) }, 500);
+// }
+
+// function second(){
+//   console.log(2);
+
+// }
+
+// first()
+// second()
+
+
+// //скормили функцию функции
+
+// function learnJS(lang, callback) {
+//   console.log(`я учу ${lang}`);
+//   callback();
+// }
+
+
+// function done() {
+//   console.log('ya krosavcheg');
+// }
+
+// learnJS('JavaScript', done);
