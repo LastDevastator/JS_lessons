528a2b807b8bf6920fcf2ce44b401b80691c25e6
Oleksii Ryzhov <ryzhov@ukr.net>
2023-08-21 16:16:28 +0300

    28th lesson added

---
+++ src/JS/JS-lesson-28.strings_numb.js
@@ -0,0 +1,10 @@
+// методы - вспомогательные функции
+// свойства - вспомогательные значения
+
+const str = 'test'
+
+console.log(str.length) // .lenght - свойства. пишутся через точку
+console.log(str[2])  // можно получить символ по индексу, но не можем так менять значания
+
+console.log(str.toLocaleUpperCase) // метод увеличения регистра
+console.log(str.toLocaleLowerCase) // метод уменьшения регистра
