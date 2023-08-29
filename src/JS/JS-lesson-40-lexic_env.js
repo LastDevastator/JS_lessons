61adbaf55b16dd41e81a8a115a0ccf3405913c9e
Oleksii Ryzhov <oleksii.ryzhov@gmail.com>
2023-08-29 20:37:09 +0300

    42nd

--- src/JS/JS-lesson-40-lexic_env.js
+++ src/JS/JS-lesson-40-lexic_env.js
@@ -1,17 +1,19 @@
-function foo(a,b) {
-    const [first] = a;
-    const {eng} = b;
+// function foo(a,b) {
+//     const [first] = a;
+//     const {eng} = b;
  
-    return `${first} ${eng}`;
-}
+//     return `${first} ${eng}`;
+// }
  
-const result = foo(['Hello', 'Привет'], { ru: 'Мир', eng: 'World' })
+// const result = foo(['Hello', 'Привет'], { ru: 'Мир', eng: 'World' })
 
-console.log(result)
+// console.log(result)
 
-let time = '';
-5 > 3 || 2 ? time = 'Day' : time = 'Night' 
-console.log(time)
+// let time = '';
+// 5 > 3 || 2 ? time = 'Day' : time = 'Night' 
+// console.log(time)
 
-console.log(3 && 4)
-console.log(3 || 4)
+// console.log(3 && 4)
+// console.log(3 || 4)
+
+console.log(2 && 1 && undefined && true && null )
